a2e2ab2e24755fe82c40f2f1c00834dc8f6296e5
Nomango <nomango@qq.com>
2022-04-01 12:33:24 +0800

    update font

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './assets/css/reset.css'
-import './assets/css/fonts.css'
+// import './assets/css/fonts.css'
 import './assets/css/hanla.css'
 import App from './App'
 import Resume from './Resume'
